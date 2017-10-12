8aeadc1aac2af36a30e3d89eff451b07df800207
Mohamed Elafifi <maelafifi@Mohameds-MacBook-Pro.local>
2017-10-11 21:02:14 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './USF_logo_green.jpg';
+import logo2 from './frn2.jpg';
+import './App.css';
+import Form from './Form.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <img src={logo2} className="App-logo" alt="logo2"/>
+        </div>
+        <Form />
+      </div>
+    );
+  }
+}
+export default App;
